4a7d3e1f6e0b92195bd689f209d5c602fb33dbb8
asheshsinha0 <75121565+asheshsinha0@users.noreply.github.com>
2024-05-17 12:21:39 +0000

    string conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -59,14 +59,14 @@
 
 // conversion of a datatype into boolean
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(typeof isLoggedIn);  // o/p -> number
 // console.log(typeof booleanIsLoggedIn) // o/p -> boolean
 
-console.log(isLoggedIn);  // o/p -> 1
-console.log(booleanIsLoggedIn) // o/p -> true
+// console.log(isLoggedIn);  // o/p -> 1
+// console.log(booleanIsLoggedIn) // o/p -> true
 
 
 // output for the below value after converted into boolean
@@ -79,5 +79,42 @@ console.log(booleanIsLoggedIn) // o/p -> true
     
 */
 
+// ******************* Operations **************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 3);
+// console.log(3 % 2);
+
+let str1 = "Hello"
+let str2 = " Ashesh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2); // 12
+// console.log(1+"2"); // 12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 //it is possibe but not recommended. Readabiltiy is more important
 
 
+
+
+// let gameCounter = 100
+// gameCounter++ 
+// console.log(gameCounter);
+
+// link to study postfix and prefix
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
